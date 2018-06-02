1424a7ad708c6914c31a7d53a91aada60373212c
Michael <michael.bretagne@gmail.com>
2018-06-02 10:47:18 -0700

    Add enzyme to project and write tests

---
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import { BurgerBuilder } from "./BurgerBuilder";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+configure({ adapter: new Adapter() });
+
+describe("<BurgerBuilder />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<BurgerBuilder onInitIngredients={() => {}} />);
+  });
+
+  it("should render <BuildControls /> when receiving ingredients", () => {
+    wrapper.setProps({ ings: { salad: 0 } });
+    expect(wrapper.find(BuildControls)).toHaveLength(1);
+  });
+
+  it("should NOT render <BuildControls /> when NOT receiving ingredients", () => {
+    wrapper.setProps({ ings: null });
+    expect(wrapper.find(BuildControls)).toHaveLength(0);
+  });
+});
